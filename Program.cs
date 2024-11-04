6f2c97f89a1f9afe9b1f2e2f3470221b92ae788f
frizzle101101 <aaronfritz96@gmail.com>
2024-11-04 15:21:39 -0500

    Commit Solution

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿namespace _6_Zigzag_Conversion {
+    internal class Program {
+        static void Main(string[] args) {
+            Solution solution = new Solution();
+        }
+    }
+}
---
+++ Solution.cs
@@ -0,0 +1,30 @@
+﻿public class Solution {
+    public string Convert(string s, int numRows) {
+        var sarr = s.ToCharArray();
+        var rows = new Dictionary<int, List<char>>();
+        for (int i = 0; i < numRows; i++) {
+            rows[i] = new List<char>();
+        }
+        int row = 0;
+        bool dirPlus = true;
+        for (int i = 0; i < s.Length; i++) {
+            rows[row].Add(sarr[i]);
+            if (row == numRows - 1)
+                dirPlus = false;
+            else if (row == 0)
+                dirPlus = true;
+
+            if (numRows > 1) {
+                if (dirPlus)
+                    row++;
+                else
+                    row--;
+            }
+        }
+        string rtn = "";
+        foreach (var chars in rows) {
+            rtn += new string(chars.Value.ToArray());
+        }
+        return rtn;
+    }
+}
